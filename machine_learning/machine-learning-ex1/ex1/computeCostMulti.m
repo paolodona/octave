632236d0e68f70bbd81260c41a0029daeac2f430
Paolo Dona <paolo.dona@gmail.com>
2017-05-27 22:28:02 +0100

    vectorised computeCostMulti

--- machine_learning/machine-learning-ex1/ex1/computeCostMulti.m
+++ machine_learning/machine-learning-ex1/ex1/computeCostMulti.m
@@ -6,16 +6,21 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predictions = X * theta;
+# 1st method
+# sqrErrors = (predictions - y) .^ 2; % squared errors
+# J = 1/(2*m) * sum(sqrErrors);
 
-
-
+# 2nd method
+# takes advantage of matrix product to square
+J = 1/(2*m) * (predictions - y)' * (predictions - y);
 
 % =========================================================================
 
