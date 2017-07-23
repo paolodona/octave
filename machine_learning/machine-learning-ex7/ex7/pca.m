84d2b2fc44a342cb1d3034bd0cec095ceb4c6668
Paolo Dona <paolo.dona@gmail.com>
2017-07-23 18:47:56 +0100

    dimensionality reduction and recovery

--- machine_learning/machine-learning-ex7/ex7/pca.m
+++ machine_learning/machine-learning-ex7/ex7/pca.m
@@ -14,17 +14,15 @@ S = zeros(n);
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should first compute the covariance matrix. Then, you
 %               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
+%               and eigenvalues of the covariance matrix.
 %
 % Note: When computing the covariance matrix, remember to divide by m (the
 %       number of examples).
 %
 
-
-
-
-
-
+Sigma = (X'*X) ./ m; # covariance matrix
+#Sigma = cov(X,1);  # for efficiency, use built in function
+[U S V] = svd(Sigma);
 
 % =========================================================================
 
